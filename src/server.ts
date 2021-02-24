372f9f435d0c98908136cb756cd4659b7b510b78
CarolineCunha <caroline.cunha.b@hotmail.com>
2021-02-24 20:56:33 -0300

    Comentario

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,11 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { router } from './routes';
 
 const app = express();
 
-//http://localhost:3333/users
-
-app.get("/", (request, response) => {
-    return response.json({message: "Hello Wolrd"})
-});
-
-//1 param => rota (recurso api)
-//2 param => request, response
-app.post("/", (request, response) => {
-    return response.json({ message: "Os dados foram salvos com sucesso!"})
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
